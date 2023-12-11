d89d1b0ec3a72f072d05825358fc681faff34ac0
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-12-11 23:53:20 +0100

    generate angular services and implement authentication

---
+++ generated-angular-services/model/models.ts
@@ -0,0 +1,7 @@
+export * from './authenticationRequest';
+export * from './authenticationResponse';
+export * from './collectionModelEntityModelEmployee';
+export * from './collectionModelEntityModelEmployeeEmbedded';
+export * from './employee';
+export * from './entityModelEmployee';
+export * from './link';
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {HomeComponent} from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to the login page if the path is empty
+  { path: 'login', component: LoginComponent },
+  {
+    path:'dashboard',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
